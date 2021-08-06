4c04a2b877b319c53480c158590071329ac1e4b5
Caliman Stefan Daniel <46399590+brasilnike@users.noreply.github.com>
2021-08-06 09:14:48 +0300

    Added the project to source control

---
+++ Online Platform/Models/DataAccess/Open_Connection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Platform.Models.DataAccess
+{
+    public static class Open_Connection
+    {
+        public static SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(@"Server=localhost;Database=Online Platform;Trusted_Connection=Yes;Integrated Security=SSPI;");
+            }
+        }
+    }
+}
---
+++ Online Platform/ViewModels/Student/StudentVM.cs
@@ -0,0 +1,60 @@
+﻿using Online_Platform.Models.BusinessLogic;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Online_Platform.ViewModels
+{
+    class StudentVM: INotifyPropertyChanged
+    {
+        public StudentBusinessLogic studentBL { get; set; }
+        public List<string> MarksList { get; set; }
+        public List<string> AbsenteesList { get; set; }
+        public List<string> FinalMarkList { get; set; }
+        public List<string> SubjectMaterialsList { get; set; }
+
+        public StudentVM()
+        {
+            studentBL = new StudentBusinessLogic();
+            MarksList = new List<string>();
+            foreach(var list in studentBL.subject_marks.Values.ToList())
+            {
+                string grades_subject = null;
+                foreach (var elem in list)
+                    grades_subject += elem.Grade +", ";
+               MarksList.Add(grades_subject);
+            }
+            AbsenteesList = new List<string>();
+            foreach(var list in studentBL.subject_absentees.Values.ToList())
+            {
+                var absentees = new List<string>();
+                foreach (var elem in list)
+                {
+                    string[] date = elem.Absentee_Date.ToString().Split(' ');
+                    absentees.Add(elem.Is_Motivated ? date[0] + 'M' : date[0] + 'N');
+                }
+                AbsenteesList.Add(string.Join(", ", absentees));
+            }
+            FinalMarkList = new List<string>();
+            foreach(var elem in studentBL.subject_final_mark.Values.ToList())
+            {
+                FinalMarkList.Add(elem.ToString());
+            }
+            SubjectMaterialsList = new List<string>();
+            foreach(var elem in studentBL.subjects.ToList())
+            {
+                SubjectMaterialsList.Add(elem.Materials.ToString());
+            }
+            studentBL.general_mark = studentBL.subject_final_mark.Values.Sum() / FinalMarkList.Count;
+
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
